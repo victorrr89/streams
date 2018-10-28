dfc2673953b1d991a3edacf4c44e99c817e66d65
Victor Brenister <victor.brenisthter@gmail.com>
2018-10-28 14:25:07 +0200

    Init commit, added required files

---
+++ src/test/java/edu/java8/practice/java8practice/DemoTest.java
@@ -0,0 +1,14 @@
+package edu.java8.practice.java8practice;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DemoTest {
+
+    @Test
+    public void test() {
+        assertThat(true).isTrue();
+    }
+
+}
